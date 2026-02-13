d1587517d40789b05dcc19089f8f59a7da4fe717
Nia Williams <niayoshe@gmail.com>
2026-02-13 14:27:18 -0500

    feat: Add network block

--- dwmblocks/src-patched/blocks.def.h
+++ dwmblocks/src-patched/blocks.def.h
@@ -1,13 +1,15 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
-//#define SCRIPT_PATH "test"
+// Modify this file to change what commands output to your statusbar, and
+// recompile using the make command. #define SCRIPT_PATH "test"
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", SCRIPT_PATH "/battery.sh",	1,		0},
-	{"", SCRIPT_PATH "/weather.sh",	1800,		0},
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    {"", SCRIPT_PATH "/network.sh", 5, 0},
+    {"", SCRIPT_PATH "/battery.sh", 1, 0},
+    {"", SCRIPT_PATH "/weather.sh", 1800, 0},
+    {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
+    {"", "date '+%b %d (%a) %I:%M%p'", 5, 0},
 };
 
-//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+// sets delimiter between status commands. NULL character ('\0') means no
+// delimiter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
